d0a56b16bbe84d1309d548d2ebdb0864d202dc76
Jose Estrada <j.estrada49@icloud.com>
2021-09-20 14:54:23 -0700

    Add a session management middleware

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,14 @@ const router = express.Router()
 
 module.exports = params => {
   router.get('/', (req, res) => {
+
+    /* // Prove session with Private Browser
+    if( !req.session.visitcout ) {
+      req.session.visitcout = 0
+    }
+    req.session.visitcout += 1
+    console.log(`Number of visits: ${req.session.visitcout}`) */
+
     res.render('pages/index', { pageTitle: 'Welcome' })
   })
 
