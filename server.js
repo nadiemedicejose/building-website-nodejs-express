c25c17c2e9e0dedeaa5327c5e5f2d49fbdd20c3c
Jose Estrada <j.estrada49@icloud.com>
2021-09-19 23:36:45 -0700

    Serving HTML files and static content

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const { response } = require('express');
-const express = require('express');
+const express = require('express')
+const path = require('path')
+
 const app = express()
 
 const port = 3000
 
+// Use middleware to serve static files
+app.use(express.static(path.join(__dirname, './static')))
+
 app.get('/', (req, res) => {
-  res.send('Hello Express :)')
+  res.sendFile(path.join(__dirname, './static/index.html'))
 })
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'))
+})
+
+
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`)
