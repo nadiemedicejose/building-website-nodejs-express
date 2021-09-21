9186c20e775a5b66903bf45cba5d9d979f7212a5
Jose Estrada <j.estrada49@icloud.com>
2021-09-20 18:04:24 -0700

    Partials and lists

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ module.exports = params => {
 
   router.get('/', async (req, res) => {
     const topSpeakers = await speakersService.getList()
-    res.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers })
+    const artwork = await speakersService.getAllArtwork()
+    res.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers, artwork })
   })
 
   router.use('/speakers', speakersRoute(params))
